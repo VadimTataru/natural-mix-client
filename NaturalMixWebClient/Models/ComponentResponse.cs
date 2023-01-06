049ced39a4304ca7a0e00c2f2775db6c2031c3d3
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2023-01-06 19:54:11 +0300

    feat: fetch request from api

---
+++ NaturalMixWebClient/Models/ComponentResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace NaturalMixWebClient.Models
+{
+    public class ComponentResponse
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+        [JsonProperty("Description")]
+        public string? Description { get; set; }
+        [JsonProperty("Origin")]
+        public string? Origin { get; set; }
+        [JsonProperty("Naturalness")]
+        public float? Naturalness { get; set; }
+    }
+}
