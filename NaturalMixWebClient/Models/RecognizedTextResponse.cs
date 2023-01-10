380715a79e9e66c2ee15de54e88d463d6c2f3366
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2023-01-10 11:20:10 +0300

    fix: move image recognizing to server

---
+++ NaturalMixWebClient/Models/RecognizedTextResponse.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace NaturalMixWebClient.Models
+{
+    public class RecognizedTextResponse
+    {
+        [JsonProperty("RecognizedText")]
+        public string Text { get; set; }
+        [JsonProperty("Data")]
+        public List<ComponentResponse> Data { get; set; }
+    }
+}
